1edf9061b2bc0f4bd5302f97ddc1bf3caad66bc6
GuaiYiHu <guaiyihu@foxmail.com>
2020-04-09 13:30:36 +0530

    whyred: keylayout: Map Button Jack keylayout to fix headset button controls
     * input: sdm660-snd-card Button Jack as /devices/soc/soc:sound/sound/card0/input8

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
