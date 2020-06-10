92d28d196461e433a965ca264d728d2597e121bc
darkbeast69 <guptaaryan189@gmail.com>
2020-06-10 16:30:15 +0530

    tulip: Checkout audio configs from jasmine_sprout

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(DEVICE_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
 
 # ConsumerIR
