be5d8a43eaf23e6f488fac2dc36131ffb218e94a
Aryan Gupta <guptaaryan189@gmail.com>
2020-07-26 10:16:09 +0000

    tulip: enable updatable APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,9 @@ DEVICE_PATH := device/xiaomi/tulip
 # Assert
 TARGET_OTA_ASSERT_DEVICE := tulip
 
+#APEX
+TARGET_ENABLE_APEX := true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
